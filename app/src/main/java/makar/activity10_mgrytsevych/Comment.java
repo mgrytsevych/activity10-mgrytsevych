5486e60e04d059a8e29ca5788cc7bf726747294f
mgrytsevych <mgrytsevych@css.edu>
2017-05-15 17:53:02 -0500

    Initial commit

---
+++ app/src/main/java/makar/activity10_mgrytsevych/Comment.java
@@ -0,0 +1,49 @@
+package makar.activity10_mgrytsevych;
+
+/**
+ * Created by Makar on 3/31/2017.
+ * Contains data to be used in database
+ */
+
+public class Comment{
+    private long id;
+    private String comment;
+    private String rating;
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    //retrieves and returns comment id
+    public long getId() {
+        return id;
+    }
+
+    //sets the id
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    //retrieves and returns comment
+    public String getComment() {
+        return comment;
+    }
+
+    //sets the comment to a string
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    /**
+     *used by the ArrayAdapter in the ListView
+     *returns comment
+     */
+    @Override
+    public String toString() {
+        return (comment + "The rating is " + rating);  //displays the rating along with the comment text
+    }
+}
---
+++ app/src/main/java/makar/activity10_mgrytsevych/CommentsDataSource.java
@@ -0,0 +1,80 @@
+package makar.activity10_mgrytsevych;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Makar on 5/15/2017.
+ *  maintains the database connection
+ * and supports adding new comments and fetching them
+ */
+
+public class CommentsDataSource {
+    // Database fields
+    private SQLiteDatabase database;
+    private MySQLiteHelper dbHelper;
+    private String[] allColumns = { MySQLiteHelper.COLUMN_ID,
+            MySQLiteHelper.COLUMN_COMMENT };
+    //reference to database
+    public CommentsDataSource(Context context) {
+        dbHelper = new MySQLiteHelper(context);
+    }
+
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+    //closes database helper
+    public void close() {
+        dbHelper.close();
+    }
+    //creates and adds comment to db
+    public Comment createComment(String comment) {
+        ContentValues values = new ContentValues();
+        values.put(MySQLiteHelper.COLUMN_COMMENT, comment);
+        long insertId = database.insert(MySQLiteHelper.TABLE_COMMENTS, null,
+                values);
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_COMMENTS,
+                allColumns, MySQLiteHelper.COLUMN_ID + " = " + insertId, null,
+                null, null, null);
+        cursor.moveToFirst(); //points to first comment
+        Comment newComment = cursorToComment(cursor);
+        cursor.close(); //closes the cursor
+        return newComment;
+    }
+    //removes comments from db
+    public void deleteComment(Comment comment) {
+        long id = comment.getId();
+        System.out.println("Comment deleted with id: " + id);
+        database.delete(MySQLiteHelper.TABLE_COMMENTS, MySQLiteHelper.COLUMN_ID
+                + " = " + id, null);
+    }
+    //retrieves all comments in a column in a db
+    public List<Comment> getAllComments() {
+        List<Comment> comments = new ArrayList<Comment>();
+
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_COMMENTS,
+                allColumns, null, null, null, null, null);
+        //adds all the comments starting from first
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            Comment comment = cursorToComment(cursor);
+            comments.add(comment);
+            cursor.moveToNext();
+        }
+        // make sure to close the cursor
+        cursor.close();
+        return comments;
+    }
+    //points cursor to comment
+    private Comment cursorToComment(Cursor cursor) {
+        Comment comment = new Comment();
+        comment.setId(cursor.getLong(0));
+        comment.setComment(cursor.getString(1));
+        return comment;
+    }
+}
